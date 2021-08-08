bc2b978bdb8d6084608921e45b4d5502c8e68311
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-08-08 22:52:54 +0200

    refactor

--- client/App.js
+++ client/App.js
@@ -1,71 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Text, View } from "react-native";
 import { AppRegistry } from "react-native";
-import io from "socket.io-client";
 
 export default function App() {
-    const [message, setMessage] = useState("");
-    const [messagesList, setMessagesList] = useState([]);
-
-    const socket = io("http://localhost:5000", {
-        jsonp: false,
-        transports: ["websocket"],
-    });
-
-    useEffect(() => {
-        socket.on("chat message", (msg) => {
-            console.log(msg);
-            setMessagesList([...messagesList, msg]);
-        });
-    }, [messagesList]);
-
-    const chatMessages = messagesList.map((chatMessage, index) => (
-        <Text style={styles.chatText} key={index}>
-            {chatMessage}
-        </Text>
-    ));
-
-    const sendChatMessage = () => {
-        socket.emit("chat message", message);
-        setMessage("");
-    };
-
     return (
-        <View style={styles.container}>
-            {chatMessages}
-            <TextInput
-                style={styles.chatBox}
-                autoCorrect={false}
-                value={message}
-                onChangeText={(message) => setMessage(message)}
-                onSubmitEditing={() => sendChatMessage()}
-            />
+        <View>
+            <Text></Text>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    chatBox: {
-        height: 40,
-        borderWidth: 2,
-        top: 600,
-        padding: 5,
-        margin: 10,
-        borderRadius: 12,
-    },
-    chatText: {
-        borderWidth: 1,
-        borderRadius: 12,
-        backgroundColor: "skyblue",
-        textAlign: "center",
-        minWidth: 60,
-        padding: 5,
-        marginBottom: 5,
-    },
-});
---
+++ client/pages/Chat.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import io from "socket.io-client";
+
+const Chat = () => {
+    const [message, setMessage] = useState("");
+    const [messagesList, setMessagesList] = useState([]);
+
+    const socket = io("http://localhost:5000", {
+        jsonp: false,
+        transports: ["websocket"],
+    });
+
+    useEffect(() => {
+        socket.on("chat message", (msg) => {
+            console.log(msg);
+            setMessagesList([...messagesList, msg]);
+        });
+    }, [messagesList]);
+
+    const chatMessages = messagesList.map((chatMessage, index) => (
+        <Text style={styles.chatText} key={index}>
+            {chatMessage}
+        </Text>
+    ));
+
+    const sendChatMessage = () => {
+        socket.emit("chat message", message);
+        setMessage("");
+    };
+
+    return (
+        <View style={styles.container}>
+            {chatMessages}
+            <TextInput
+                style={styles.chatBox}
+                autoCorrect={false}
+                value={message}
+                onChangeText={(message) => setMessage(message)}
+                onSubmitEditing={() => sendChatMessage()}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    chatBox: {
+        height: 40,
+        borderWidth: 2,
+        top: 600,
+        padding: 5,
+        margin: 10,
+        borderRadius: 12,
+    },
+    chatText: {
+        borderWidth: 1,
+        borderRadius: 12,
+        backgroundColor: "skyblue",
+        textAlign: "center",
+        minWidth: 60,
+        padding: 5,
+        marginBottom: 5,
+    },
+});
+
+export default Chat;
